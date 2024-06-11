9fc631c1f200a7127f6a5114a78678e696ffb72d
syddyd <sydney.dacks@gmail.com>
2024-06-11 14:20:59 -0700

    fixed move speed, added basic platforms

---
+++ Assets/Scripts/PlatformLift.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLift : MonoBehaviour
+{
+    private float leftSide;
+    private float rightSide;
+    private float surface;
+    Vector2 target;
+    GameObject player;
+    Collider2D thisCollider;
+    
+    void Awake()
+    {
+        leftSide = transform.position.x - transform.lossyScale.x / 2;
+        rightSide = transform.position.x + transform.lossyScale.x / 2;
+        surface = transform.position.y + transform.lossyScale.y / 2 + 0.1f;
+        thisCollider = gameObject.GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            if (player.transform.position.x < leftSide)
+            {
+                target = new Vector2(leftSide, surface);
+            }
+            else if (player.transform.position.x > rightSide)
+            {
+                target = new Vector2(rightSide, surface);
+            }
+            else
+            {
+                target = new Vector2(player.transform.position.x, surface);
+            }
+            
+            StartCoroutine(LiftPlayer(target));
+        }
+    }
+
+    private IEnumerator LiftPlayer(Vector2 target)
+    {
+        print("lifting");
+        player.GetComponent<PlayerController>().setMoveable(false);
+        thisCollider.isTrigger = true;
+        while (Math.Abs(player.transform.position.x - target.x) > 0.2 || Math.Abs(player.transform.position.y - target.y) > 0.2)
+        {
+            print("passed condition");
+            Vector3.MoveTowards(player.transform.position, target, Time.deltaTime);
+            yield return null;
+        }
+        thisCollider.isTrigger = false;
+        player.GetComponent<PlayerController>().setMoveable(true);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,32 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.iOS;
 using HutongGames.PlayMaker;
 using Unity.VisualScripting;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     private Transform trans;
     private float moveDirection;
+    private float groundMove;
+    private float airMove;
+    private bool moveAble;
 
     [Range(0.1f, 10f)] public float jumpPower;
     public float fallMultiplier = 1.1f;
-    [Range(0.1f, 10f)] public float moveSpeed;
+    [Range(0.1f, 100f)] public float moveSpeed;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         trans = rb.transform;
+        moveAble = true;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveDirection = context.ReadValue<Vector2>().x;
-        if (!isGrounded())
-        {
-            moveDirection /= 3f;
-        }
+        groundMove = context.ReadValue<Vector2>().x;
+        airMove = groundMove/2f;
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -52,10 +54,12 @@ public class PlayerController : MonoBehaviour
     }
 
     //Method which accounts for the case where the player presses button too early 
-    private IEnumerator JumpMemorizer(InputAction.CallbackContext context) 
+    private IEnumerator JumpMemorizer(InputAction.CallbackContext context)
     {
-        for (int i = 0; i < 30; i ++){
-            if (isGrounded()){
+        for (int i = 0; i < 30; i++)
+        {
+            if (isGrounded())
+            {
                 print("jump retry");
                 Jump(context);
                 break;
@@ -66,13 +70,26 @@ public class PlayerController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if (rb.velocity.y < 0)
+        if (moveAble)
         {
-            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y + fallMultiplier * Physics.gravity.y * Time.deltaTime);
+            if (!isGrounded())
+            {
+                moveDirection = airMove;
+            } else {
+                moveDirection = groundMove;
+            }
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y + fallMultiplier * Physics.gravity.y * Time.deltaTime);
+            }
+            else
+            {
+                rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+            }
         }
         else
         {
-            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+            rb.velocity = Vector2.zero;
         }
     }
 
@@ -80,5 +97,10 @@ public class PlayerController : MonoBehaviour
     {
         //Debug.DrawLine(trans.position, new Vector3(trans.position.x, trans.position.y -10f, trans.position.z), Color.red);
         return Physics2D.Raycast(trans.position, Vector2.down, 1.6f, 8);
+    }
+
+    public void setMoveable(bool move)
+    {
+        moveAble = move;
     }
 }
